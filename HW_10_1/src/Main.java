218b4fa157156511a20140c5835ae7744de4dae6
DmitriyKuzmenko22 <dp190792kdg@gmail.com>
2016-12-12 21:04:05 +0200

    dz

---
+++ HW_10_1/src/Main.java
@@ -0,0 +1,20 @@
+/*Создать объект класса Фотоальбом, используя классы Фотография, Страница.
+Методы: задать название фотографии, дополнить фотоальбом фотографией,
+вывести на консоль количество фотографий.
+*/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+
+//        Albom albom = new Albom("her");
+//        albom.addPhotoOnPage(1,"gavno");
+//        albom.addPhotoOnPage(2,"gavno");
+//        albom.setPhotoTitle("af", 2,1);
+//
+//        System.out.println(albom.proverkaFoto());
+Kvadrat kvadrat=new Kvadrat(5,5,"gold");
+        System.out.println(kvadrat.toString());
+    }
+}
+
---
+++ HW_10_1/src/Tochka.java
@@ -0,0 +1,33 @@
+/*Создать объект класса Квадрат, используя классы Точка, Отрезок.
+Методы: задание размеров, растяжение, сжатие, поворот, изменение цвета.*/
+public class Tochka {
+    private int x;
+    private int y;
+
+    public Tochka(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+
+        return x;
+    }
+
+    public void setX(int x) {
+
+        this.x = x;
+    }
+
+    public int getY() {
+
+        return y;
+    }
+
+    public void setY(int y) {
+
+        this.y = y;
+    }
+
+
+}
