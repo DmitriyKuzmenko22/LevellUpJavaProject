0b339e79dc1ab7204696094b007dd7a8a03c91a3
DmitriyKuzmenko22 <dp190792kdg@gmail.com>
2016-12-15 21:19:31 +0200

    ормоh

--- HW_11/src/com/company/Main.java
+++ HW_11/src/com/company/Main.java
@@ -21,5 +21,7 @@ public class Main {
         String un = n%100 < 20 ? units[n%100] : tens[n%100/10] + " " + units[n%10];
         String th = n/1000%100 < 20 ? units[n%100] + " thousand(s)" : hundreds[n/1000/100%10] + " " + tens[n/1000%100/10] + " " + units[n/1000%10];
         return th + " " + hundreds[n/100%10] + " " +un;
-    }
+   //jjj
+        //
+        }
 }
