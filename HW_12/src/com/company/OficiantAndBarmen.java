d26906e7bedfaac3d1e8c02da2a8394eb45003a6
Дмитрий <dp190792kdg@gmail.com>
2017-01-07 11:38:59 +0200

    ww

---
+++ HW_12/src/com/company/OficiantAndBarmen.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class OficiantAndBarmen {
+    private String name;
+    private int age;
+
+    public OficiantAndBarmen(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
