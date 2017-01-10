fc267c47351d58d56606ed30da03a190cecbd106
remdp <remmdp@GMAIL.COM>
2017-01-10 19:43:19 +0200

    less

---
+++ java16.6/address_book/src/view/Dialog.java
@@ -0,0 +1,71 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by java on 10.01.2017.
+ */
+public abstract class Dialog extends JDialog {
+
+    protected JButton ok = null;
+    protected JButton cancel=null;
+
+    protected JPanel panel = new JPanel();
+    private final ActionListener listener;
+
+    private boolean isOkPresed = false;
+
+
+    public Dialog() {
+      setModal(true);
+        setLayout(null);
+        setBounds(300,400,250,240);
+        setResizable(false);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+        add(panel);
+        panel.setBounds(0,0,250,240);
+
+        panel.setLayout(null);
+
+        this.listener=getButtonListener();
+
+        this.ok = createActionButton("ok",new Rectangle(30,160,80,30));
+        this.cancel = createActionButton("cancel",new Rectangle(130,160,80,30));
+
+        panel.add(ok);
+        panel.add(cancel);
+    }
+
+    public boolean isOkPresed() {
+        boolean res = isOkPresed;
+        isOkPresed=false;
+        return res;
+    }
+
+    public void setOkPresed(boolean okPresed) {
+        isOkPresed = okPresed;
+    }
+
+    public abstract Object getEntity();
+
+    private JButton createActionButton(String name, Rectangle rectangle) {
+        JButton btn=new JButton(name);
+        btn.addActionListener(listener);
+        btn.setBounds(rectangle);
+
+        return btn;
+    }
+
+    private ActionListener getButtonListener() {
+        return e -> {
+            if (e.getActionCommand().equals("ok")){
+                isOkPresed=true;
+            }
+            dispose();
+        };
+    }
+}
