d4f6fe0a4f4082d20a1b7a023917197e706aa0fb
remdp <remmdp@GMAIL.COM>
2017-01-27 21:03:11 +0200

    sosi pisos

---
+++ Lesson21/src/three/Main.java
@@ -0,0 +1,20 @@
+package three;
+
+/**
+ * Created by java on 27.01.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+//        int n = 487;
+//    }
+//        public int recurs(int n){
+//            if (n<=10) return n;
+//            return (n/10)+n%10;
+
+        Tree<Integer> tree = new BinaryTree<>(Integer::compare);
+
+    }
+
+    }
+
