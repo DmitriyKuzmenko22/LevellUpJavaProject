6fefa1c40382906ae3727234092ce4cbc7f8c6e1
remdp <remmdp@GMAIL.COM>
2017-02-03 20:57:30 +0200

    acteal

--- lesso23/src/collection/main.java
+++ lesso23/src/collection/main.java
@@ -3,39 +3,51 @@ package collection;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedList;
 
 /**
  * Created by java on 03.02.2017.
  */
 public class main {
     public static void main(String[] args) {
-        Collection list1= new ArrayList<>();
-        Collection list2= new ArrayList<>();
-
-        for (int i=1;i<=10;i++){
-            list1.add(i);
-        }
-
-        list1.add(132);
-        list2.add(132);
-
-        for (int i = 20; i <= 25; i++){
-            list2.add(i);
-        }
-
-        Collection list3=new ArrayList<>(list1);
-        list3.addAll(list2);
-
-
-
-
-        System.out.println(list1.size());
-        System.out.println(list2.size());
-        System.out.println(list3.size());
-
-        Integer[] array = (Integer[]) list1.toArray(new Integer[list1.size()]);
-        System.out.println(Arrays.toString(array));
+//        Collection list1= new ArrayList<>();
+//        Collection list2= new ArrayList<>();
+//
+//        for (int i=1;i<=10;i++){
+//            list1.add(i);
+//        }
+//
+//        list1.add(132);
+//        list2.add(132);
+//
+//        for (int i = 20; i <= 25; i++){
+//            list2.add(i);
+//        }
+//
+//        Collection list3=new ArrayList<>(list1);
+//        list3.addAll(list2);
+//
+//
+//
+//
+//        System.out.println(list1.size());
+//        System.out.println(list2.size());
+//        System.out.println(list3.size());
+//
+//        Integer[] array = (Integer[]) list1.toArray(new Integer[list1.size()]);
+//        System.out.println(Arrays.toString(array));
 //
 
+
+        LinkedList<Integer> list=new LinkedList<>();
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        list.add(6);
+        list.add(5);
+        list.add(4);
+
+        System.out.println(list.get(3));
+        System.out.println(list.get(5));
     }
 }
